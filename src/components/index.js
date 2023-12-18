b197f6915cdf560dda97c7302a7ef9b1d5c0e441
Nurseyit Abdyldaev <nurseyit2005@gmail.com>
2023-12-18 15:03:14 +0600

    second

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,30 @@ export { default as Sidebar } from './Sidebar'
 export { default as Videos } from './Videos'
 export { default as VideoCard } from './VideoCard'
 export { default as ChannelCard } from './ChannelCard'
+
+
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://youtube-v31.p.rapidapi.com/search',
+  params: {
+    q: 'music',
+    part: 'snippet,id',
+    regionCode: 'US',
+    maxResults: '50',
+    order: 'date'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'a75af92f6emsh85af13ba9fac022p163ddejsnac3b8df2db8d',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
